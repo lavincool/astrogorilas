4a28380602da176175225d7ecf0e1ef0e537c611
karlajmz <karlajmzrdz03@gmail.com>
2024-10-05 14:11:28 -0500

    holi ahora es color rojo

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,7 +4,7 @@ import CSS from "./style.module.css";
 export default function Page() {
   return (
     <div className={`${CSS.chat} m-6`}>
-      <h1 className="text-2xl mb-3">chat</h1>
+      <h1 className="text-2xl mb-3" style={{ color: "red" }}>chat</h1>
       <div className={CSS.cuadrado}>cuadradito</div>
       <div className="w-48">
         <Button className="mt-3 w-full" color="success" variant="bordered">

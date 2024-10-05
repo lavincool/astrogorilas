2c689d97c6bbe2858fc69de28df75f85db7bdba5
Angel Lavin <lavindev@proton.me>
2024-10-05 18:16:52 -0500

    Added Heroicons and CheckBadge on the top of main page

--- src/app/(public)/Cards.tsx
+++ src/app/(public)/Cards.tsx
@@ -1,12 +1,16 @@
 import { Card, CardBody } from "@nextui-org/react";
 import IndividualCard from "./components/Card";
+import { CheckBadgeIcon } from "@heroicons/react/24/outline";
 
 export default function Cards() {
   return (
     <div>
       <Card className="m-3 border-3 border-green-500">
         <CardBody className="text-center">
-          <p>El cultivo se encuentra bien :)</p>
+          <div className="flex align-middle items-center m-auto">
+            <CheckBadgeIcon className="w-6 h-6 mr-2 text-green-400" />
+            <p className="text-large">El cultivo se encuentra bien :)</p>
+          </div>
         </CardBody>
       </Card>
       <div className="flex flex-wrap gap-3 justify-center">

bea9707d846cf49ce7e2bc0f9009132bd315f6bd
Angel Lavin <lavindev@proton.me>
2024-10-05 13:52:10 -0500

    Added NextUI

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@nextui-org/react";
 import CSS from "./style.module.css";
 
 export default function Page() {
   return (
-    <div className={CSS.chat}>
-      chat
+    <div className={`${CSS.chat} m-6`}>
+      <h1 className="text-2xl mb-3">chat</h1>
       <div className={CSS.cuadrado}>cuadradito</div>
+      <div className="w-48">
+        <Button className="mt-3 w-full" color="success" variant="bordered">
+          Click me
+        </Button>
+      </div>
     </div>
   );
 }

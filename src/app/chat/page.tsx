17afc1a13d963fc97972d8829f62303478258e0b
Angel Lavin <lavindev@proton.me>
2024-10-05 13:40:31 -0500

    Added example page

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,10 @@
+import CSS from "./style.module.css";
+
+export default function Page() {
+  return (
+    <div className={CSS.chat}>
+      chat
+      <div className={CSS.cuadrado}>cuadradito</div>
+    </div>
+  );
+}

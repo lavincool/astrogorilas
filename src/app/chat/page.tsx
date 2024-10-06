22e57d0661277162cf61204c15c45a69ca4f291f
karlajmz <karlajmzrdz03@gmail.com>
2024-10-05 20:27:17 -0500

    xd

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,16 +1,19 @@
-import { Button } from "@nextui-org/react";
+import { Button, Spinner } from "@nextui-org/react";
 import CSS from "./style.module.css";
 
 export default function Page() {
   return (
     <div className={`${CSS.chat} m-6`}>
-      <h1 className="text-2xl mb-3" style={{ color: "red" }}>chat</h1>
+      <h1 className="text-2xl mb-3" style={{ color: "red" }}>
+        chat
+      </h1>
       <div className={CSS.cuadrado}>cuadradito</div>
       <div className="w-48">
         <Button className="mt-3 w-full" color="success" variant="bordered">
           Click me
         </Button>
       </div>
+      <Spinner />
     </div>
   );
 }
